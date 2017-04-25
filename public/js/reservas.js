6af104bf61cb0ba52ccb3a092275df4916fdb008
fran0813 <fran0813@hotmail.com>
2017-04-25 13:58:13 -0500

    cambios

--- public/js/reservas.js
+++ public/js/reservas.js
@@ -1,3 +1,39 @@
+$( document ).ready(function() {
+
+	$.ajax({
+		method: "POST",
+		url: "/js/mostrarLocalidad.php",
+		dataType: 'json',
+	})
+
+	.done(function(response) {
+		$('#localidad').html(response);
+	});
+
+});
+
+function mostrarCanchas() {
+
+	var idlocal = $("#localidad").val();
+
+	$.ajax({
+		method: "POST",
+		url: "/js/mostrarCanchasSelect.php",
+		dataType: 'json',
+		data: { idlocal: idlocal}
+	})
+
+	.done(function(response) {
+		$('#cancha').html(response);
+	});
+
+}
+
+function d(){
+	var a = $("#datetime").val();
+	alert(a);
+}
+
 // $("#form").on("submit", function(){
 
 // 	var username = $("#username").val();
--- resources/views/reservas.blade.php
+++ resources/views/reservas.blade.php
@@ -7,6 +7,14 @@
 		<div class="container">
 			<div class="col-sm-12 col-md-12 col-lg-12">
 				<form id="form">
+					<select id="localidad" onchange=" mostrarCanchas()">
+					<option>Seleccione una localidad</option>
+					</select>
+					<select id="cancha"><option>Seleccione una cancha</option></select>
+					<input type="date" id="date" value="<?php echo date("Y-m-d");?>">
+					<input type="time" id="time" value="<?php echo date("H:i");?>">
+					<input type="datetime-local"  id="datetime" value="<?php echo date("Y-m-d\TH:i");?>">
+					<input type="button" value="enviar" onclick="d()">
 				</form>
 			</div>
 		</div>
@@ -15,6 +23,6 @@
 
 	@section('javascript')
 		
-		<script src="{{ asset('js/canchas.js') }}"></script>
+		<script src="{{ asset('js/reservas.js') }}"></script>
 
 	@endsection
