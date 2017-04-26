39547411d6fa93ce2c05a9a5153aab734c72260c
fran0813 <fran0813@hotmail.com>
2017-04-25 19:48:21 -0500

    cambios

--- public/js/ingresarReserva.php
+++ public/js/ingresarReserva.php
@@ -1,4 +1,4 @@
-<!-- <?php
+<?php
 
 	$name = $_POST['name'];
 
@@ -7,37 +7,28 @@
 		$conn = new PDO('mysql:host=localhost; dbname=netplay', "root", "12345");
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		$sql = $conn->prepare('SELECT * FROM users');
-		$sql->execute(array('name' => $name, 'email' => $email, 'password' => $password));
-		$resultados = $sql->fetchAll();
-		$html = "";
-		$ban = False;
-		
-		foreach ($resultados as $resultado) {
-			$comprobar = $resultado['name'];
 
-			if($comprobar == $name) {
-				$ban = True;
-				break;
-			}
-			
-		};
-
-		if($ban == False){
-
-		$sql = $conn->prepare('INSERT INTO users VALUES (null, :name, :email, :password, null, null, null)');
+		$sql = $conn->prepare('INSERT INTO reserva VALUES (null, :horaReserva, "A", null, "1")');
 		$sql->bindParam("name", $name, PDO::PARAM_STR);
-		$sql->bindParam("email", $email, PDO::PARAM_STR);
-		$sql->bindParam("password", $password, PDO::PARAM_STR);
+
 		$sql->execute();
 
-		$html .= "<p>Ingreso</p>";
+		$sql = $conn->prepare('SELECT * FROM reserva ORDER BY id DESC LIMIT 1');
+		$sql->execute();
+		$resultados = $sql->fetchAll();
 
-		}elseif($ban == True){
-			$html .= "<p>Usuario ya esta en la bd</p>";
-		}
+		foreach ($resultados as $resultado) {
+			$id = $resultado['id'];
+		};
 
-		echo json_encode($html);
+		$sql = $conn->prepare('INSERT INTO reservasCanchas VALUES (null, :horaInicial, :horaFinal, :id, :idcancha)');
+		$sql->bindParam("id", $id, PDO::PARAM_STR);
+
+		$sql->execute();
+
+		$html = "";
+	
+		$html .= "<p>Ingreso</p>"
 
 
 
@@ -48,4 +39,4 @@
 
 	}
 
-?> -->
+?>
