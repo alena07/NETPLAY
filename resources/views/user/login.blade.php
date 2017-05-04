3e25a7919aabd1a177f1a3b49c7bcfa2f545d5e6
Angie-Espitia <anie-sichaca@upc.edu.co>
2017-05-04 17:20:01 -0500

    registrar estilo

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">	
 
-	<title>@yield('title')</title>
+	<title>Login</title>
 
 	{{-- Botstrap --}}
 	<link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
